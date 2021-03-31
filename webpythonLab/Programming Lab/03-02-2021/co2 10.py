d0c9ad9b783f2dd71bf7bc879924255c0f1fe258
aravindvv0 <aravindvv2022a@mca.ajce.in>
2021-03-31 19:38:55 +0530

    Web

--- webpythonLab/Programming Lab/03-02-2021/co2 10.py
+++
@@ -1,9 +0,0 @@
-print("\n Generate all factors of a number \n")
-
-
-
-n = int(input("Enter a number : "))
-print(" The factors of",+n,"are : ")
-for i in range(1,n+1):
-    if n%i==0:
-        print(i)
--- webpythonLab/Programming Lab/27-01-2021/19.py
+++
@@ -1,9 +0,0 @@
-import math
-print("\n")
-print("Enter the two numbers to find the gcd")
-
-a = int(input())
-b = int(input())
-print("GCD of given numbers is: ")
-
-print(math.gcd(a,b))
